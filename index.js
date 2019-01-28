1c76d5414a1c0ece1c294b624e2d785b3514fcba
ykrueng <yasirah.krueng@gmail.com>
2019-01-28 16:04:24 -0600

    add implementation for enhancer success

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const e_levels = {
-  16: 'PRI',
-  17: 'DUO',
-  18: 'TRI',
-  19: 'TET',
-  20: 'PEN'
-}
+const e_levels = [
+'0',
+'1',
+'2',
+'3',
+'4',
+'5',
+'6',
+'7',
+'8',
+'9',
+'10',
+'11',
+'12',
+'13',
+'14',
+'15',
+'PRI',
+'DUO',
+'TRI',
+'TET',
+'PEN'
+]
 
 const enhancer = {
   repair: (item) => {
@@ -12,9 +28,60 @@ const enhancer = {
     newItem.durability = 100;
 
     return newItem;
+  },
+
+  success: (item) => {
+    if (!checkName(item)) throw new Error('Invalid name property');
+
+    if (!checkType(item)) throw new Error('Invalid type property');
+
+    if (!checkDurability(item)) throw new Error('Invalid durability property');
+
+    if (!checkEnhancement(item)) throw new Error('Invalid enhancement property');
+
+    const newEnhancementLevel = addEnhancement(item.enhancement);
+
+    const newItem = {
+      ...item,
+      name: updateName(newEnhancementLevel, item.name),
+      enhancement: newEnhancementLevel
+    };
+
+    return newItem;
   }
 }
 
+function checkName(item) {
+  return item.name && typeof item.name === 'string';
+}
+
+function checkType(item) {
+  return item.type && (item.type === 'weapon' || item.type === 'armor');
+}
+
+function checkDurability(item) {
+  return item.durability && item.durability > 0 && item.durability <= 100;
+}
+
+function checkEnhancement(item) {
+  return item.enhancement && typeof item.enhancement === 'string' && e_levels.includes(item.enhancement);
+}
+
+function addEnhancement(currentLevel) {
+  if (currentLevel === 'TET') return currentLevel;
+  const current = e_levels.findIndex(e => e === currentLevel);
+  return e_levels[current + 1];
+}
+
+function updateName(currentLevel, name) {
+  const strippedName = name.slice(name.indexOf(']') + 1);
+
+  const current = e_levels.findIndex(e => e === currentLevel);
+  return `[${current < 16 ? '+' : ''}${e_levels[current]}] ${strippedName}`;
+
+}
+
 module.exports = {
-  enhancer
+  enhancer,
+  e_levels
 }
--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,4 @@
-const { enhancer } = require('./index');
+const { enhancer, e_levels } = require('./index');
 
 describe('Enhancer Repair', () => {
   const item = {
@@ -14,3 +14,71 @@ describe('Enhancer Repair', () => {
     expect(enhancer.repair(item).durability).toBe(100);
   })
 });
+
+describe('Enhancer Success', () => {
+  const item = {
+    name: 'item',
+    type: 'weapon',
+    durability: 100,
+    enhancement: '0',
+  }
+
+  describe('Invalid Properties Check', () => {
+    it('should throw error for invalid name', () => {
+      expect(() => enhancer.success({})).toThrow();
+      expect(() => enhancer.success({ name: null })).toThrow();
+      expect(() => enhancer.success({ name: 1 })).toThrow();
+    });
+    
+    test('should throw error for invalid type', () => {
+      expect(() => enhancer.success({})).toThrow();
+      expect(() => enhancer.success({ type: null })).toThrow();
+      expect(() => enhancer.success({ type: 1 })).toThrow();
+      expect(() => enhancer.success({ type: 'knive' })).toThrow();
+    });
+    
+    test('should throw error for invalid durability', () => {
+      expect(() => enhancer.success({})).toThrow();
+      expect(() => enhancer.success({ durability: null })).toThrow();
+      expect(() => enhancer.success({ durability: -1 })).toThrow();
+      expect(() => enhancer.success({ durability: 190 })).toThrow();
+    });
+    
+    test('should throw error for invalid enhancement', () => {
+      expect(() => enhancer.success({})).toThrow();
+      expect(() => enhancer.success({ enhancement: null })).toThrow();
+      expect(() => enhancer.success({ enhancement: 1 })).toThrow();
+      expect(() => enhancer.success({ durability: 190 })).toThrow();
+    });
+  });
+
+  it('should return new item', () => {
+    const newItem = enhancer.success(item);
+
+    expect(enhancer.success(item)).not.toBe(item);
+    expect(typeof newItem.name).toBe('string');
+    expect(typeof newItem.type).toBe('string');
+    expect(typeof newItem.durability).toBe('number');
+    expect(typeof newItem.enhancement).toBe('string');
+  });
+
+  describe('Increase Enhancement Level', () => {
+    it('should increase enhancement level by 1', () => {
+      expect(enhancer.success({ ...item, enhancement: '0' }).enhancement).toBe('1');
+      expect(enhancer.success({ ...item, enhancement: '10' }).enhancement).toBe('11');
+      expect(enhancer.success({ ...item, enhancement: '15' }).enhancement).toBe('PRI');
+      expect(enhancer.success({ ...item, enhancement: 'DUO' }).enhancement).toBe('TRI');
+      expect(enhancer.success({ ...item, enhancement: 'TRI' }).enhancement).toBe('TET');
+      expect(enhancer.success({ ...item, enhancement: 'TET' }).enhancement).toBe('TET');
+    })
+  });
+
+  it('should update name based on Enhancement Level', () => {
+    expect(enhancer.success({ ...item, enhancement: '0' }).name).toBe('[+1] item');
+    expect(enhancer.success({ ...item, enhancement: '10' }).name).toBe('[+11] item');
+    expect(enhancer.success({ ...item, enhancement: '15' }).name).toBe('[PRI] item');
+    expect(enhancer.success({ ...item, enhancement: 'DUO' }).name).toBe('[TRI] item');
+    expect(enhancer.success({ ...item, enhancement: 'TRI' }).name).toBe('[TET] item');
+    expect(enhancer.success({ ...item, enhancement: 'TET' }).name).toBe('[TET] item');
+  })
+})
